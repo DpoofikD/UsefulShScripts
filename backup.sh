5f3846730bff328852a6638291d3240278ce5d44
vismettkl <47731321+DpoofikD@users.noreply.github.com>
2021-08-05 23:42:52 +0500

    Add files via upload

---
+++ backup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mysqldump --all-databases > /home/user/dbbackup.sql
+
+# Place tar.gz directories you want to save into /home ( tar cvf - YOUR_DIRECTORY | gzip -9c > /home/backup.tar.gz )
+
+tar cvf - /home | gzip -9c > /home/backup.tar.gz
+/usr/bin/openssl des -in /home/backup.tar.gz -out /home/backup.sec
+rm -rf /home/backup.tar.gz
